44721c18656cbfa513174ff05311ae18ac323314
Silvia <silvia.giusti@clariter.it>
2017-03-30 15:49:27 +0200

    aggiornamento costants

--- src/it/sdefri/utils/Constants.java
+++ src/it/sdefri/utils/Constants.java
@@ -2,7 +2,14 @@ package it.sdefri.utils;
 
 public class Constants {
 
-	public String LISTENING_PATH="C:";
+	public static final String LISTENING_PATH = "";
+	public static final String DOCUMENT_PATH_WIN = "C:\\template\\";
+	public static final String DOCUMENT_PATH_LINUX = "/tmp/template";
+	
+	
+	// COSTANTI IMPORTATE DA DALLAS CONTROLLARE UTILITA'
+	public static final String EURO = "EUR";
+	public static final String PROPERTIES = "";
 	
 	//MI LEGGI FANS?
 }
